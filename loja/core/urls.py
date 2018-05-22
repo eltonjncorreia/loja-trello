fbc750b5e59fab551f5b7dfa1ca7d01e0d359128
elton jefferson <eltonjncorreia@gmail.com>
2018-05-22 11:08:51 -0300

    rotas genericas

---
+++ loja/core/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import ProdutoViewSet, PedidoViewSet, CategoriaViewSet, EstoqueViewSet
+
+router = routers.DefaultRouter(trailing_slash=False)
+
+router.register(r'produtos', ProdutoViewSet)
+router.register(r'pedidos', PedidoViewSet)
+router.register(r'categorias', CategoriaViewSet)
+router.register(r'estoques', EstoqueViewSet)
+
+
+urlpatterns = [
+       path('', include(router.urls)),
+]
