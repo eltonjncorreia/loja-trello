1f7636d37100b5e1f111667bdb7ca8eb5bc58ca5
elton jefferson <eltonjncorreia@gmail.com>
2018-05-25 01:38:11 -0300

    rota remove item

--- loja/store/urls.py
+++ loja/store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from loja.store.views import home, carrinho
+from loja.store.views import home, carrinho, remove
 
 app_name = "store"
 urlpatterns = [
     path('', home, name="home"),
     path('carrinho/<uuid:pk>/', carrinho, name="carrinho"),
+    path('remove/<uuid:pk>/', remove, name="remove"),
 ]
