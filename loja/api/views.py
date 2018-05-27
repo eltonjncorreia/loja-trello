b665e325b5be05cc0fdcbd8570c2d82d4ed9ec78
elton jefferson <eltonjncorreia@gmail.com>
2018-05-27 16:16:17 -0300

    refactoring

--- loja/api/views.py
+++ loja/api/views.py
@@ -10,23 +10,23 @@ from .serializers import ProdutoSerializer, CategoriaSerializer, \
 class ProdutoViewSet(ModelViewSet):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, IsAdminUser)
+    permission_classes = (IsAuthenticatedOrReadOnly, )
 
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, IsAdminUser)
+    permission_classes = (IsAuthenticatedOrReadOnly, )
 
 
 class PedidoViewSet(ModelViewSet):
     queryset = Pedido.objects.all()
     serializer_class = PedidoSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, IsAdminUser)
+    permission_classes = (IsAuthenticatedOrReadOnly, )
 
 
 
 class EstoqueViewSet(ModelViewSet):
     queryset = Estoque.objects.all()
     serializer_class = EstoqueSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, IsAdminUser)
+    permission_classes = (IsAuthenticatedOrReadOnly, )
